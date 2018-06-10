e981e3408b9730d5ddf448d12c1653fbd6b4ffc6
nkkumawat <nk0kumawat@gmail.com>
2018-06-10 12:44:49 +0530

    voted classification

---
+++ token.py
@@ -0,0 +1,8 @@
+# import nltk
+# nltk.download()
+# nltk.download()
+# from nltk.tokenize import sent_tokenize , word_tokenize
+# # 
+# example_text = "Hllo there, how are you doing today"
+
+# print(sent_tokenize(example_text))
